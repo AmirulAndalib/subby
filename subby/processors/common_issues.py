7bbf153c2ff6bd1e9577c00951b937db9b6e3f76
vevv <68520787+vevv@users.noreply.github.com>
2026-08-16 01:50:29 +0200

    Correct no-op regex OR

--- subby/processors/common_issues.py
+++ subby/processors/common_issues.py
@@ -69,7 +69,7 @@ class CommonIssuesFixer(BaseProcessor):
             )
             # Replace hashes, asterisks at the end of a line with a musical note
             line = re.sub(
-                r'(?<=\s)(?<![#\*])(?:[#\*]{1,3}|[#\*]{1,3})(?![0-9A-Z])(</i>$|$)',
+                r'(?<=\s)(?<![#\*])(?:[#\*]{1,3})(?![0-9A-Z])(</i>$|$)',
                 r'♪\1',
                 line,
                 flags=re.M
